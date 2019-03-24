d794261e662280d289c3adb7544b5ddbf1937802
Idan Cohen <idan@idancohen.com>
2019-03-24 19:36:14 +0200

    improve user profile

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { environment } from '../../../environments/environment';
+import { Candidate } from '../../types/candidate.model';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,10 +12,18 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
+  maxVotesPerUser = environment.maxVotesPerUser;
+  candidates$: Observable<Candidate[]>;
 
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService,
+              private db: AngularFirestore) { }
 
   ngOnInit() {
+    this.candidates$ = this.auth.user$.pipe(
+      switchMap(user =>
+        this.db.collection<Candidate>('candidates', ref => ref.where(`votes.${user.uid}`, '>', 0)).valueChanges()
+      )
+    );
   }
 
 }
