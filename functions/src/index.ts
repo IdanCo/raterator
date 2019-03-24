67bb460414a691177633616c5e13777bbaef6008
Idan Cohen <idan@idancohen.com>
2019-03-24 19:01:41 +0200

    functions setup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,7 @@
-import * as functions from 'firebase-functions';
+// import * as functions from 'firebase-functions';
+
+// const admin = require('firebase-admin');
+// admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +9,21 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+// export const updateVotes = functions.firestore
+//   .document('candidates/{candidateId}')
+//   .onUpdate((change, context) => {
+//     const candidate = change.after!.data();
+//
+//     // no need to updates votes if there are no votes
+//     if (!candidate || !candidate.votes) return;
+//
+//     let totalVotes = 0;
+//
+//     Object.keys(candidate.votes).forEach(voter => {
+//       totalVotes += candidate.votes[voter];
+//     });
+//
+//     change.after.ref.update({ totalVotes });
+//
+//   });
